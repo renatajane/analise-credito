f93621bb1e67ed4228a8cc0b460323f50cb68f31
renatajane <renatajane28@gmail.com>
2024-07-16 18:16:21 -0300

    Inclui anotações

--- src/main/java/com/analisedecredito/aplicacao_analise_credito/controller/RendaFonteController.java
+++ src/main/java/com/analisedecredito/aplicacao_analise_credito/controller/RendaFonteController.java
@@ -1,5 +1,10 @@
 package com.analisedecredito.aplicacao_analise_credito.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/renda-fonte")
 public class RendaFonteController {
     
 }
--- src/main/java/com/analisedecredito/aplicacao_analise_credito/service/RendaFonteService.java
+++ src/main/java/com/analisedecredito/aplicacao_analise_credito/service/RendaFonteService.java
@@ -1,5 +1,8 @@
 package com.analisedecredito.aplicacao_analise_credito.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class RendaFonteService {
     
 }
