c2dc1b8969c69ed87cc2bd1c5e83db52e91bd29d
renatajane <renatajane28@gmail.com>
2024-08-05 19:15:14 -0300

    Implementa controller de beneficiado

--- src/main/java/com/analisedecredito/aplicacao_analise_credito/AplicacaoAnaliseCreditoApplication.java
+++ src/main/java/com/analisedecredito/aplicacao_analise_credito/AplicacaoAnaliseCreditoApplication.java
@@ -2,8 +2,10 @@ package com.analisedecredito.aplicacao_analise_credito;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.analisedecredito.aplicacao_analise_credito.config")
 public class AplicacaoAnaliseCreditoApplication {
 	
 	public static void main(String[] args) {
