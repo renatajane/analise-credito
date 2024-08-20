37f30b3bb9fae532fbb792841b2978a3685d6aa8
renatajane <renatajane28@gmail.com>
2024-08-20 10:11:49 -0300

    Remove pastas inutilizáveis

--- src/main/java/com/analisedecredito/aplicacao_analise_credito/frontend/src/App.jsx
+++ src/main/java/com/analisedecredito/aplicacao_analise_credito/frontend/src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import '@govbr-ds/core/dist/core.min.js';
 import Header from "./components/Header/Header"
 import Footer from "./components/Footer/Footer"
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
