9021077bea99694e0fee2dcc32ea1d0a79bdc0cb
Renata Jane <116691993+renatajane@users.noreply.github.com>
2024-09-09 09:01:35 -0300

    Delete node_modules directory

--- node_modules/react-datepicker/dist/es/calendar_container.d.ts
+++
@@ -1,8 +0,0 @@
-import React from "react";
-export interface CalendarContainerProps extends React.PropsWithChildren {
-    showTimeSelectOnly?: boolean;
-    showTime?: boolean;
-    className?: string;
-}
-declare const CalendarContainer: React.FC<CalendarContainerProps>;
-export default CalendarContainer;
